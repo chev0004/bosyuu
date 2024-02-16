bf1c6fcef84572195c5d2278ceda503a2feb1c10
“chev” <chevv0004@gmail.com>
2024-02-16 21:45:25 +0900

    added routes to middleware

--- middleware.ts
+++ middleware.ts
@@ -14,12 +14,30 @@ export function middleware(req: NextRequest) {
     if (!isLoggedIn && req.url == 'http://localhost:3000/profile') {
         return NextResponse.redirect('http://localhost:3000/board');
     }
+    if (!isLoggedIn && req.url == 'https://bosyuu.netlify.app/profile') {
+        return NextResponse.redirect('https://bosyuu.netlify.app/board');
+    }
+    if (!isLoggedIn && req.url == 'https://bosyuu.vercel.app/profile') {
+        return NextResponse.redirect('https://bosyuu.vercel.app/board');
+    }
 
     if (
         req.url === 'http://localhost:3000/' ||
         req.url === 'http://localhost:3000'
     ) {
         return NextResponse.redirect('http://localhost:3000/board');
+    }
+    if (
+        req.url === 'https://bosyuu.netlify.app/' ||
+        req.url === 'https://bosyuu.netlify.app'
+    ) {
+        return NextResponse.redirect('https://bosyuu.netlify.app/board');
+    }
+    if (
+        req.url === 'https://bosyuu.vercel.app/' ||
+        req.url === 'https://bosyuu.vercel.app'
+    ) {
+        return NextResponse.redirect('https://bosyuu.vercel.app/board');
     }
 
     return NextResponse.next();
