e4eba0dfd6977c7b3dc109f1b686946329516a5a
“chev” <chevv0004@gmail.com>
2024-02-16 19:26:16 +0900

    changed middleware from ts to js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest) {
+export function middleware(req) {
     const isLoggedIn = Boolean(req.cookies.get('next-auth.session-token'));
     const url = req.nextUrl;
     const { pathname } = url;
