196b9b60aecd0c7b591c6183755541bc6edb5437
“chev” <chevv0004@gmail.com>
2024-02-17 14:37:15 +0900

    updated isLoggedIn checker

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-    const isLoggedIn = Boolean(req.cookies.get('next-auth.session-token'));
+    const isLoggedIn = Boolean(
+        req.cookies.get('__Secure-next-auth.session-token')
+    );
     const url = req.nextUrl;
     const { pathname } = url;
-
-    if (req.url == 'https://bosyuu.netlify.app/profile') {
-        console.log(req.cookies.get('__Secure-next-auth.session-token'));
-    }
 
     if (pathname.startsWith(`/api/`)) {
         if (req.headers.get('sec-fetch-site') == 'none') {
@@ -18,9 +16,9 @@ export function middleware(req: NextRequest) {
     if (!isLoggedIn && req.url == 'http://localhost:3000/profile') {
         return NextResponse.redirect('http://localhost:3000/board');
     }
-    // if (!isLoggedIn && req.url == 'https://bosyuu.netlify.app/profile') {
-    //     return NextResponse.redirect('https://bosyuu.netlify.app/board');
-    // }
+    if (!isLoggedIn && req.url == 'https://bosyuu.netlify.app/profile') {
+        return NextResponse.redirect('https://bosyuu.netlify.app/board');
+    }
     if (!isLoggedIn && req.url == 'https://bosyuu.vercel.app/profile') {
         return NextResponse.redirect('https://bosyuu.vercel.app/board');
     }
