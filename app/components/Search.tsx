56df7ab75be2e8e38e2eec0cd6e239995c89561a
“chev” <chevv0004@gmail.com>
2024-02-19 20:05:10 +0900

    added search icon

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import { IoSearchSharp } from 'react-icons/io5';
 
 const Search = () => {
     const router = useRouter();
@@ -18,12 +19,18 @@ const Search = () => {
             onKeyDown={handleKeyDown}
             className='h-24 flex items-center justify-center'
         >
-            <input
-                name='query'
-                placeholder='Search (username, description, or a tag)'
-                autoComplete='off'
-                className='bg-darkMain rounded-md focus:ring-0 focus:outline-none h-10 w-8/12 p-4 text-white'
-            />
+            <div className='flex items-center justify-center w-8/12 relative'>
+                <IoSearchSharp
+                    color='white'
+                    className='absolute right-4 h-5 w-5'
+                />
+                <input
+                    name='query '
+                    placeholder='Search (username, description, or a tag)'
+                    autoComplete='off'
+                    className='bg-darkMain rounded-md focus:ring-0 focus:outline-none h-10 w-full p-4 text-white'
+                />
+            </div>
         </form>
     );
 };
