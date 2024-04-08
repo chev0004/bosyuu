bbaaa76018e34d3556e69df1eb0472e8cf37afe5
“chev” <chevv0004@gmail.com>
2024-04-08 19:13:59 +0900

    re-added register prompt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import Navbar from './components/Navbar';
-// import RegisterPrompt from './components/RegisterPrompt';
 import { Rokkitt } from 'next/font/google';
 import { AuthProvider } from './Providers';
 import { authOptions } from '@/authOptions';
 import { getServerSession } from 'next-auth';
 import NextTopLoader from 'nextjs-toploader';
+import { fetchVictimData } from '@/libs/getVictim';
+import RegisterPrompt from './components/RegisterPrompt';
 
 const rokkitt = Rokkitt({ subsets: ['latin'] });
 
@@ -26,21 +27,22 @@ const RootLayout = async ({
 }: Readonly<{
     children: React.ReactNode;
 }>) => {
+    const victimData = await fetchVictimData();
     const session = await getServerSession(authOptions);
     let colour: string = '#707070';
-    const victimGender = session?.user?.profile?.gender;
+    const victimGender = victimData?.gender;
     victimGender == '1'
         ? (colour = '#c1d5e9')
         : victimGender === '2'
         ? (colour = '#f9a8d5')
         : (colour = '#707070');
-    return ( 
+    return (
         <html lang='en'>
             <AuthProvider>
                 <body className={`${rokkitt.className} bg-back`}>
                     <NextTopLoader color={colour} showSpinner={false} />
                     <Navbar icon={session?.user?.image} />
-                    {/* {session && <RegisterPrompt victimData={victimData} />} */}
+                    {session && <RegisterPrompt victimData={victimData} />}
                     {children}
                 </body>
             </AuthProvider>
