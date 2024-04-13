9381e3ce0d8679680d47a4988fcf06b65eb932fd
chev <chevv0004@gmail.com>
2024-04-13 15:56:50 +0900

    fixed scroll gutter offset when viewing profile

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,42 +12,42 @@ import RegisterPrompt from './components/RegisterPrompt';
 const rokkitt = Rokkitt({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: 'Bosyuu',
-    description: 'Discord friend bulletin board',
-    keywords: [
-        'Discord',
-        'Discord friends',
-        'Discord bulletin board',
-        'Discord friend board',
-    ],
+	title: 'Bosyuu',
+	description: 'Discord friend bulletin board',
+	keywords: [
+		'Discord',
+		'Discord friends',
+		'Discord bulletin board',
+		'Discord friend board',
+	],
 };
 
 const RootLayout = async ({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode;
 }>) => {
-    const victimData = await fetchVictimData();
-    const session = await getServerSession(authOptions);
-    let colour: string = '#707070';
-    const victimGender = victimData?.gender;
-    victimGender == '1'
-        ? (colour = '#c1d5e9')
-        : victimGender === '2'
-        ? (colour = '#f9a8d5')
-        : (colour = '#707070');
-    return (
-        <html lang='en'>
-            <AuthProvider>
-                <body className={`${rokkitt.className} bg-back`}>
-                    <NextTopLoader color={colour} showSpinner={false} />
-                    <Navbar icon={session?.user?.image} />
-                    {session && <RegisterPrompt victimData={victimData} />}
-                    {children}
-                </body>
-            </AuthProvider>
-        </html>
-    );
+	const victimData = await fetchVictimData();
+	const session = await getServerSession(authOptions);
+	let colour: string = '#707070';
+	const victimGender = victimData?.gender;
+	victimGender == '1'
+		? (colour = '#c1d5e9')
+		: victimGender === '2'
+		? (colour = '#f9a8d5')
+		: (colour = '#707070');
+	return (
+		<html lang="en">
+			<AuthProvider>
+				<body className={`${rokkitt.className} bg-back`}>
+					<NextTopLoader color={colour} showSpinner={false} />
+					<Navbar icon={session?.user?.image} />
+					{session && <RegisterPrompt victimData={victimData} />}
+					{children}
+				</body>
+			</AuthProvider>
+		</html>
+	);
 };
 
 export default RootLayout;
